69a82fbe6aa1aea64d0761dd96ee68a0fb2aada8
V-Burton97 <victor.burton@hotmail.fr>
2024-10-03 12:35:08 +0200

    Connection à trueLayer puis redirection sur app fonctionnelle.

--- full_flutter/lib/main.dart
+++ full_flutter/lib/main.dart
@@ -1,16 +1,41 @@
-// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
 
 import 'package:flutter/material.dart';
 import 'package:full_flutter/pages/outcome_page.dart';
 import 'package:full_flutter/pages/sort_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:full_flutter/pages/database.dart';
-import 'package:provider/provider.dart'; 
+import 'package:provider/provider.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 import 'pages/income_pages.dart';
 import 'pages/spent.dart';
 
-// import 'package:shared_preferences/shared_preferences.dart';
+final FlutterAppAuth appAuth = FlutterAppAuth();
+final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
+
+const String clientId = 'sandbox-trackspent-f42a7b';
+const String redirectUri = 'trackspent://auth/callback';
+const String issuer = 'https://auth.truelayer-sandbox.com/';
+final List<String> scopes = [
+  'info',
+  'accounts',
+  'balance',
+  'cards',
+  'transactions',
+  'direct_debits',
+  'standing_orders',
+  'offline_access',
+];
+final String scope = scopes.join(' ');
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +44,7 @@ Future<void> main() async {
       providers: [
         ChangeNotifierProvider(create: (_) => SpentModel()),
       ],
-      child: const MyApp(),
+      child: MyApp(),
     ),
   );
 }
@@ -34,15 +59,92 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _currentIndex = 0;
   bool _isAuthenticated = false;
-
+  bool _isAuthenticatedWithTrueLayer = false;
+  StreamSubscription? _sub;
 
   @override
   void initState() {
     super.initState();
+    _handleIncomingLinks();
+    _checkInitialLink();
     _checkAuthentication();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Provider.of<SpentModel>(context, listen: false).initializeDummyData();
     });
+  }
+
+  void _checkInitialLink() async {
+    try {
+      final initialUri = await getInitialUri();
+      if (initialUri != null) {
+        print('Lien initial reçu : $initialUri');
+        if (initialUri.scheme == 'trackspent' &&
+            initialUri.host == 'auth' &&
+            initialUri.path == '/callback') {
+          final code = initialUri.queryParameters['code'];
+          if (code != null) {
+            _exchangeAuthorizationCode(code);
+          }
+        }
+      }
+    }  catch (err) {
+      print('Erreur lors de la réception du lien initial : $err');
+    }
+  }
+
+
+  void _handleIncomingLinks() {
+    // Écoute des liens entrants
+    _sub = uriLinkStream.listen((Uri? uri) {
+      print('Lien entrant reçu : $uri');
+      if (uri != null && uri.scheme == 'trackspent') {
+        if (uri.host == 'auth' && uri.path == '/callback') {
+          final code = uri.queryParameters['code'];
+          if (code != null) {
+            _exchangeAuthorizationCode(code);
+          }
+        }
+      }
+    }, onError: (Object err) {
+      print('Erreur lors de la réception du lien : $err');
+    });
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _exchangeAuthorizationCode(String code) async {
+    // Votre code pour échanger le code d'autorisation contre un jeton d'accès
+    final response = await http.post(
+      Uri.parse('https://auth.truelayer-sandbox.com/connect/token'),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: {
+        'grant_type': 'authorization_code',
+        'client_id': clientId,
+        'redirect_uri': redirectUri,
+        'code': code,
+        // N'incluez pas client_secret pour les clients publics
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final accessToken = data['access_token'];
+      final refreshToken = data['refresh_token'];
+
+      await secureStorage.write(key: 'access_token', value: accessToken);
+      await secureStorage.write(key: 'refresh_token', value: refreshToken);
+
+      setState(() {
+        _isAuthenticatedWithTrueLayer = true;
+      });
+    } else {
+      print('Échec de l\'échange du code : ${response.body}');
+      // Gérer l'erreur (afficher un message, etc.)
+    }
   }
 
   Future<void> _checkAuthentication() async {
@@ -52,20 +154,76 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _isAuthenticated = token != null;
     });
+
+    if (_isAuthenticated) {
+      // Vérifiez si l'utilisateur est authentifié avec TrueLayer
+      final accessToken = await secureStorage.read(key: 'access_token');
+      if (accessToken == null) {
+        setState(() {
+          _isAuthenticatedWithTrueLayer = false;
+        });
+      } else {
+        setState(() {
+          _isAuthenticatedWithTrueLayer = true;
+        });
+      }
+    }
   }
 
-  setCurrentIndex(int index) {
+
+  setCurrentIndex(int index) async {
     setState(() {
       _currentIndex = index;
     });
   }
 
+Future<void> _authenticateWithTrueLayerManual() async {
+  final authorizationUrl = Uri.https(
+    'auth.truelayer-sandbox.com',
+    '/',
+    {
+      'response_type': 'code',
+      'client_id': clientId,
+      'redirect_uri': redirectUri,
+      'scope': scope,
+      'providers': 'uk-cs-mock uk-ob-all uk-oauth-all',
+      'state': 'random_state_string',
+    },
+  );
+
+  final urlString = authorizationUrl.toString();
+
+  // if (await canLaunch(urlString)) {
+  //   await launch(urlString);
+  // } else {
+  //   throw 'Could not launch $urlString';
+  // }
+  try {
+    final result = await FlutterWebAuth.authenticate(
+      url: urlString,
+      callbackUrlScheme: 'trackspent',
+    );
+
+    final code = Uri.parse(result).queryParameters['code'];
+    if (code != null) {
+      await _exchangeAuthorizationCode(code);
+    }
+  } catch (e) {
+    print('Erreur lors de l\'authentification : $e');
+  }
+}
+
   Future<void> _logout() async {
-  final prefs = await SharedPreferences.getInstance();
+    final prefs = await SharedPreferences.getInstance();
     await prefs.remove('auth_token');
+
+    // Supprimez les tokens TrueLayer
+    await secureStorage.delete(key: 'access_token');
+    await secureStorage.delete(key: 'refresh_token');
 
     setState(() {
       _isAuthenticated = false;
+      _isAuthenticatedWithTrueLayer = false;
     });
   }
 
@@ -77,6 +235,23 @@ class _MyAppState extends State<MyApp> {
         home: AuthPage(), // Redirige vers la page d'authentification
       );
     }
+
+    if (!_isAuthenticatedWithTrueLayer) {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Connexion TrueLayer'),
+          ),
+          body: Center(
+            child: ElevatedButton(
+              onPressed: _authenticateWithTrueLayerManual,
+              child: const Text('Connecter mon compte bancaire'),
+            ),
+          ),
+        ),
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
--- full_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ full_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,9 @@ import Foundation
 
 import cloud_firestore
 import firebase_core
+import flutter_appauth
+import flutter_secure_storage_macos
+import flutter_web_auth
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -16,6 +19,9 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
